00cdfe3d6dc2e823509cdb784ad9c06ee37bab01
Arpit Tyagi <arpit.tyagi@wnco.com>
2020-04-12 19:08:21 -0500

    1.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
-import { S3Support } from "./aws/s3-support";
-import { SQSSupport } from "./aws/sqs-support";
+import {SQSSupport} from "./aws/sqs-support";
+import {S3Support} from "./aws/s3-support";
+import S3 from "aws-sdk/clients/s3";
+import SQS from "aws-sdk/clients/sqs";
 
 module.exports = {
-    S3 : S3Support,
-    SQS : SQSSupport
-}
-
+    SQS: SQSSupport,
+    S3: S3Support,
+    SQSSupport: SQSSupport,
+    S3Support: S3Support,
+    defaultSQSInstance: (region?: string) => {
+        SQSSupport.setRegion(region);
+        return new SQSSupport();
+    },
+    defaultS3Instance: (region?: string) => {
+        S3Support.setRegion(region);
+        return new S3Support();
+    },
+    sqsInstance: (sqs: SQS, region?: string) => {
+        SQSSupport.setRegion(region);
+        return new SQSSupport(sqs);
+    },
+    s3Instance: (s3: S3, region?: string) => {
+        S3Support.setRegion(region);
+        return new S3Support(s3);
+    },
+};
